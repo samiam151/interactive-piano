51ecb001abe4004314cb6b58148c4274f9e914a1
Nicolas Sam <samiam151@gmail.com>
2015-10-29 18:32:19 -0400

    Initial Commit

--- scripts/piano.js
+++ scripts/piano.js
@@ -1,26 +1,48 @@
 var piano = (function(){
+   var context, bufferLoader, source1, source2;
 
-   // var testVar = "private";
-   // function consoleLogTest(){
-   //    console.log(testVar);
+   // function initSounds(){
+   //    context = new (window.AudioContext || window.webkitAudioContext)();
+   //    bufferLoader = new BufferLoader(
+   //       context,
+   //       [
+   //          'assets/tropez.mp3',
+   //          'assets/twoCities.mp3'
+   //       ],
+   //       finishedLoading
+   //    );
+   //    bufferLoader.load();
    // }
-   var context, bufferLoader;
 
-   function initSounds(){
-      context = new (window.AudioContext || window.webkitAudioContext)();
+   
+   
+   // function finishedLoading(bufferList) {
+   //   console.log('made it here');
+   //   console.log(bufferList[0]);
 
-   }
+   //   // Create two sources and play them both together.
+   //   source1 = context.createBufferSource();
+   //   source2 = context.createBufferSource();
+   //   source1.buffer = bufferList[0];
+   //   source2.buffer = bufferList[1];
 
-   function playSound(event){
-      var target = event.target.dataset.key;
-      console.log(target, typeof target);
-   }
+   //   source1.connect(context.destination);
+   //   source2.connect(context.destination);
+ 
+   //    source1.start(0); 
+   //    return source1;
+   // }
 
 
+   // function playSound(event){
+   //    var target = event.target.dataset.key;
+   //    console.log(target, typeof target);
+   // }
 
-   return {
-      playSound : playSound,
-      test : consoleLogTest
-   };
+   // return {
+   //    source1 : source1,
+   //    initSounds : initSounds,
+   //    playSound : playSound,
+   // };
 
 }());
--- scripts/script.js
+++ scripts/script.js
@@ -2,9 +2,10 @@ var $ = function(selector){
    return document.querySelector(selector);
 };
 
+piano.initSounds();
+
 $('ul').addEventListener('click', function(event){
    if (event.target.tagName === 'LI') {
       piano.playSound(event);
-      piano.test();
    };
 });
