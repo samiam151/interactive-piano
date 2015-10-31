f7c5befa58beb72c62c51cbfa29a3e245317f989
Nick Sam <samiam151@gmail.com>
2015-10-30 23:14:52 -0400

    Added function to find supported audio format for the users browser
    	modified:   scripts/piano.js
    	modified:   scripts/script.js
    	modified:   styles/piano.css
    	modified:   styles/piano.css.map
    	modified:   styles/piano.scss
    	modified:   styles/style.css
    	modified:   styles/style.css.map
    
    	modified:   todo.txt

--- scripts/piano.js
+++ scripts/piano.js
@@ -4,29 +4,76 @@ var piano = (function(){
    function init(){
       context = new (window.AudioContext || window.webkitAudioContext)();
 
+      if(!context){
+         alert('Web Audio API not supported.');
+      }
+
       var request = new XMLHttpRequest();
-      var url = 'assets/twoCities.wav' || 'assets/twoCities.mp3'
+      var url = ('assets/twoCities' || 'assets/twoCities') + getCompatibleExtension();
       request.open('GET', url ,true);
       request.responseType = 'arraybuffer';
      
       request.onload = function(){
          var audioData = request.response;
+         //------------------------------------------
          console.log(audioData);
+         //------------------------------------------
          context.decodeAudioData(audioData, function(buf){
             buffer = buf;
-            console.log('buffer is ' + buffer);
+            //------------------------------------------
+            console.log('buffer ready');
+            //------------------------------------------
          });
       };
       
       request.send();
    }
 
+   function getCompatibleExtension(){
+     // Declare some variables
+     var formats = [
+       'audio/mpeg',
+       'audio/mp3',
+       'audio/wav',
+       'audio/ogg'
+       ],
+       ext = '', 
+       results = [],
+       i = 0, j = formats.length,  
+       audio = new Audio();
+     
+     // Make the function that test for browser compatibility
+     function canPlay(type){
+       return audio.canPlayType(type);
+     }
+     
+     // Loop through the sound formats to see which ones are good
+     for(; i < j; i++){
+       if(canPlay(formats[i]) === "maybe"){
+         results.push(formats[i]);
+       }
+     }
+     
+     // Get the appropriate extension
+     switch(results[0]) {
+       case "audio/mpeg":
+       case "audio/mp3":
+         return '.mp3';
+       case "audio/wav":
+       case "audio/wave":
+         return '.wav';
+       case "audio/ogg":
+         return '.ogg';
+     }
+   }
+
    function playSound(){
-      source = context.createBufferSource();
-         console.log("source = " + source);
+      var source = context.createBufferSource();
+      var gainNode = context.createGain();
+      gainNode.gain.value = 0.75;
       source.buffer = buffer;
-         console.log("source.buffer = " + source.buffer);
-      source.connect(context.destination);
+      source.connect(gainNode);
+      gainNode.connect(context.destination);
       source.start(0);
          console.log('source is playing');
    }
--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,9 @@ var $ = function(selector){
    return document.querySelector(selector);
 };
 
-window.onload = piano.init();
+window.addEventListener('load', function(){
+   piano.init();
+});
 
 $('ul').addEventListener('click', function(event){
    if (event.target.tagName === 'LI') {
