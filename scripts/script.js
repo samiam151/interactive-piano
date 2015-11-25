16346e9ed6bab631d6ede44277bb9f64ebf6df66
Nick Sam <samiam151@gmail.com>
2015-11-24 20:38:36 -0500

    Now the sounds play when you press the keyboard keys
    	modified:   scripts/piano.js
    	modified:   scripts/script.js
    
    	modified:   assets/.DS_Store
    	modified:   assets/mp3/A1.mp3
    	modified:   assets/mp3/B1.mp3
    	modified:   assets/mp3/Bb1.mp3
    	modified:   assets/mp3/C1.mp3
    	modified:   assets/mp3/C2.mp3
    	modified:   assets/mp3/Cs1.mp3
    	modified:   assets/mp3/Cs2.mp3
    	modified:   assets/mp3/D1.mp3
    	modified:   assets/mp3/D2.mp3
    	modified:   assets/mp3/E1.mp3
    	modified:   assets/mp3/E2.mp3
    	modified:   assets/mp3/Eb1.mp3
    	modified:   assets/mp3/Eb2.mp3
    	modified:   assets/mp3/F1.mp3
    	modified:   assets/mp3/Fs1.mp3
    	modified:   assets/mp3/G1.mp3
    	modified:   assets/mp3/Gs1.mp3
    	modified:   assets/wav/A1.wav
    	modified:   assets/wav/B1.wav
    	modified:   assets/wav/Bb1.wav
    	modified:   assets/wav/C1.wav
    	modified:   assets/wav/C2.wav
    	modified:   assets/wav/Cs1.wav
    	modified:   assets/wav/Cs2.wav
    	modified:   assets/wav/D1.wav
    	modified:   assets/wav/D2.wav
    	modified:   assets/wav/E1.wav
    	modified:   assets/wav/E2.wav
    	modified:   assets/wav/Eb1.wav
    	modified:   assets/wav/Eb2.wav
    	modified:   assets/wav/F1.wav
    	modified:   assets/wav/Fs1.wav
    	modified:   assets/wav/G1.wav
    	modified:   assets/wav/Gs1.wav
    	modified:   scripts/piano.js
    	modified:   scripts/script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,35 @@
 (function(){
 
+   //======================================
+   // UTILITY SELECTION FUNCTION
    var $ = function(selector){
       return document.querySelector(selector);
    };
+   //======================================
 
+   // LOAD ALL THE SOUNDS, LOADING SCREEN
    window.addEventListener('load', function(){
       piano.init();
       setTimeout(clearLoadingScreen, 1200);
    });
 
+   // MAKE THE SOUNDS PLAY WHEN YOU CLICK ON THE PIANO KEYS
    $('ul').addEventListener('click', function(event){
       if (event.target.tagName === 'LI') {
          piano.playSound(event);
       };
    });
 
-   var clearLoadingScreen = function(){
+   // MAKE THE SOUNDS PLAY WHEN YOU PRESS THE KEYPAD
+   $('body').addEventListener('keydown', respond, false);
+
+   function respond(e){
+      var key = String.fromCharCode(e.keyCode);
+      piano.playSound(e, key);
+   }
+
+   // FN: CLEARS THE LOADING SCREEN
+   function clearLoadingScreen(){
       $('.pianoWrapper').style.display = "initial";
       $('.loadingScreen').style.display = "none";
 
